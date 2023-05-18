65be652c8701aea8bac8f351367bb99eeffa2719
Matin-kch <matinkch79@gmail.com>
2023-05-18 17:13:30 +0330

    header

--- practical_group_1402_2/personal/components/header/header.vue
+++ practical_group_1402_2/personal/components/header/header.vue
@@ -1,9 +1,48 @@
 <template>
+  <div>
+    <div class="border text-center   p-5 mb-5  bg-body rounded-5">
+      <div>
+        <h1 class=" title">
+          وبسایت شخصی مطالعاتی
+        </h1>
+      </div>
 
+      <div class="text p-1">
+        <a class="text-decoration-none link-dark  ms-4" href="/">
+          خانه
+        </a>
+        <a class="text-decoration-none link-dark ms-4" href="/samples">
+          نمونه کارها
+        </a>
+
+        <a class=" text-decoration-none link-dark ms-4" href="/contact">
+          تماس با من
+        </a>
+        <a class="text-decoration-none link-dark ms-4" href="/blog">
+          وبلاگ
+        </a>
+        <a class="text-decoration-none link-dark  ms-4" href="/about">
+          درباره من
+        </a>
+        <a class="text-decoration-none link-dark  ms-4" href="/skills">
+          مهارت های من
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
-<script>
 
-</script>
-<style scoped>
+<script></script>
 
+<style>
+.title {
+  letter-spacing: 15px;
+}
+a {
+  font-size: 20px;
+  color: black;
+}
+.border {
+  box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.2);
+}
 </style>
