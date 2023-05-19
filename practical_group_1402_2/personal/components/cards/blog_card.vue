dd8a3d39fc9a1b83aaa7710aef1f6c3dd01c7f99
Matin-kch <matinkch79@gmail.com>
2023-05-19 15:52:07 +0330

    blog cards

--- practical_group_1402_2/personal/components/cards/blog_card.vue
+++ practical_group_1402_2/personal/components/cards/blog_card.vue
@@ -1,9 +1,19 @@
 <template>
-
+  <div>
+    <div class="border ">
+      <img :src="image" alt="blog image" />
+      <div class="p-5">
+        <h5 class="text-center">{{ title }}</h5>
+        <p class="text-center">{{ description }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-
+export default{
+  props: ["image", "title", "description"]
+};
 </script>
 <style scoped>
-
+/*  */
 </style>
